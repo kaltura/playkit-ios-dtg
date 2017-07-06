64ed03bcccbaf299eb266e1262ebcd8d97d1f63f
Noam Tamim <noamtm@gmail.com>
2017-07-06 22:27:11 +0300

    Initial project with Framework.

---
+++ DTGLib/DTGLib/DTGLib.h
@@ -0,0 +1,19 @@
+//
+//  DTGLib.h
+//  DTGLib
+//
+//  Created by Noam Tamim on 06/07/2017.
+//  Copyright © 2017 Kaltura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DTGLib.
+FOUNDATION_EXPORT double DTGLibVersionNumber;
+
+//! Project version string for DTGLib.
+FOUNDATION_EXPORT const unsigned char DTGLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DTGLib/PublicHeader.h>
+
+
