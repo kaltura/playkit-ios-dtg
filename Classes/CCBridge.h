6c12870ea0df81dcc0ad265bf6f75afa1389a7f2
Noam Tamim <noamtm@gmail.com>
2017-07-10 08:51:07 +0300

    Add CCBridge: a bridge for using CommonCrypto from Swift.
    It exposes one function we care about: md5(string) -> string.

---
+++ Classes/CCBridge.h
@@ -0,0 +1,15 @@
+//
+//  CCBridge.h
+//  Pods
+//
+//  Created by Noam Tamim on 10/07/2017.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCBridge : NSObject
+
++(nonnull NSString*) md5WithString:(nonnull NSString*)str;
+    
+@end
