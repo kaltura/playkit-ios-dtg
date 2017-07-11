df0b0195bebdb2ab102b5d83cd58925eab50d117
Gal Orlanczyk <gal.orlanczyk@outlook.com>
2017-07-11 09:31:22 +0300

    Initial downloader implementation

---
+++ Classes/General/Queue.swift
@@ -0,0 +1,78 @@
+//
+//  Queue.swift
+//  DownloadToGoSample
+//
+//  Created by Gal Orlanczyk on 10/07/2017.
+//  Copyright © 2017 Kaltura. All rights reserved.
+//
+
+import Foundation
+
+struct Queue<T> {
+    
+    /// The physical array that holds queue elements
+    fileprivate var array = [T?]()
+    /// The index of the first element
+    fileprivate var head = 0
+    /// The percentage of nil objects from array size to reorder queue elements (O(n) so it is better to only reorder only from some point)
+    var queueOptimizationPercentage: Double
+    /// The array minimum size to start reordering elements when certain percentage is met.
+    var queueOptimizationMinimumSize: Int
+    
+    init(arrayOptimizationMinimumSize: Int = 50, arrayOptimizationPercentage: Double = 0.25) {
+        self.queueOptimizationMinimumSize = arrayOptimizationMinimumSize
+        self.queueOptimizationPercentage = arrayOptimizationPercentage
+    }
+    
+    var isEmpty: Bool {
+        return self.count == 0
+    }
+    
+    var count: Int {
+        return self.array.count - self.head
+    }
+    
+    mutating func enqueue(_ element: T) {
+        self.array.append(element)
+    }
+    
+    mutating func enqueue(_ elements: [T]) {
+        self.array.append(contentsOf: elements as [T?])
+    }
+    
+    mutating func enqueueAtHead(_ element: T) {
+        self.array.insert(element, at: self.head)
+    }
+    
+    mutating func enqueueAtHead(_ elements: [T]) {
+        self.array.insert(contentsOf: elements as [T?], at: head)
+    }
+    
+    mutating func dequeue() -> T? {
+        guard self.head < self.array.count, let element = self.array[head] else { return nil }
+        
+        self.array[self.head] = nil
+        self.head += 1
+        
+        let percentage = Double(self.head)/Double(self.array.count)
+        if self.array.count > self.queueOptimizationMinimumSize && percentage > self.queueOptimizationPercentage {
+            self.array.removeFirst(self.head)
+            self.head = 0
+        }
+        
+        return element
+    }
+    
+    /// clears the queue of all items (like remove all)
+    mutating func purge() {
+        self.array.removeAll()
+    }
+    
+    var front: T? {
+        if self.isEmpty {
+            return nil
+        } else {
+            return self.array[self.head]
+        }
+    }
+}
