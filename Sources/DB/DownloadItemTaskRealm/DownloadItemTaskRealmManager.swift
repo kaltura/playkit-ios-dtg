e2cc441f294b762e39b159aa312dc05037ebaef6
Noam Tamim <noamtm@gmail.com>
2017-07-20 14:19:01 +0300

    Applied copyright banners.

--- Sources/DB/DownloadItemTaskRealm/DownloadItemTaskRealmManager.swift
+++ Sources/DB/DownloadItemTaskRealm/DownloadItemTaskRealmManager.swift
@@ -1,10 +1,13 @@
+// ===================================================================================================
+// Copyright (C) 2017 Kaltura Inc.
 //
-//  DownloadItemTaskRealmManager.swift
-//  Pods
+// Licensed under the AGPLv3 license, unless a different license for a 
+// particular library is specified in the applicable library path.
 //
-//  Created by Gal Orlanczyk on 16/07/2017.
-//
-//
+// You may obtain a copy of the License at
+// https://www.gnu.org/licenses/agpl-3.0.html
+// ===================================================================================================
+
 
 import Foundation
 import RealmSwift
