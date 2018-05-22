da77ab0bcf5ab4bd5a5af97ef5c65df4fdf422ec
Nilit Danan <nilit.danan@kaltura.com>
2018-05-22 14:08:24 +0300

    WIP - Changing the media player

---
+++ Example/DownloadToGo/MediaPlayerViewController.swift
@@ -0,0 +1,10 @@
+//
+//  MediaPlayerViewController.swift
+//  DownloadToGo_Example
+//
+//  Created by Nilit Danan on 5/21/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import Foundation
+import MediaPlayer
---
+++ Example/MediaPlayerFramework.framework/Headers/MediaPlayerFramework.h
@@ -0,0 +1,19 @@
+//
+//  MediaPlayerFramework.h
+//  MediaPlayerFramework
+//
+//  Created by Nilit Danan on 5/20/18.
+//  Copyright © 2018 Kaltura Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MediaPlayerFramework.
+FOUNDATION_EXPORT double MediaPlayerFrameworkVersionNumber;
+
+//! Project version string for MediaPlayerFramework.
+FOUNDATION_EXPORT const unsigned char MediaPlayerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MediaPlayerFramework/PublicHeader.h>
+
+
