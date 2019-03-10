ac7913179043569c474ba3dabf703aef43c16376
Noam Tamim <noam.tamim@kaltura.com>
2019-03-10 15:19:30 +0200

    Add travis build url as jenkins cause

--- travis-build.sh
+++ travis-build.sh
@@ -35,7 +35,7 @@ justBuild() {
   env > env.txt
   xcodebuild test -workspace DownloadToGo.xcworkspace -scheme DownloadToGo-Example -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO -destination 'platform=iOS Simulator,name=iPhone X' | tee xcodebuild.log | xcpretty -r html
   zip --junk-paths data.zip xcodebuild.log build/reports/tests.html env.txt
-  curl "$ARTIFACT_UPLOAD_URL" -Fdata.zip=@data.zip -FTRAVIS_BUILD_WEB_URL="$TRAVIS_BUILD_WEB_URL"
+  curl "$ARTIFACT_UPLOAD_URL" -Fdata.zip=@data.zip -FTRAVIS_BUILD_WEB_URL="$TRAVIS_BUILD_WEB_URL" -Fcause="$TRAVIS_BUILD_WEB_URL"
 }
 
 libLint() {
