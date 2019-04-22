9479e860ee3a5cd569030c7060cc71d3c1e7a441
Noam Tamim <noam.tamim@kaltura.com>
2019-04-22 11:43:49 +0300

    Remove trunk push from travis build

--- travis-build.sh
+++ travis-build.sh
@@ -2,17 +2,6 @@
 
 set -eou pipefail
 
-# Login to cocoapods trunk.
-login() {
-cat << EOF > ~/.netrc
-machine trunk.cocoapods.org
-  login $COCOAPODS_USERNAME
-  password $COCOAPODS_PASSWORD
-EOF
-
-chmod 0600 ~/.netrc
-}
-
 # Travis aborts the build if it doesn't get output for 10 minutes.
 keepAlive() {
   while [ -f $1 ]
@@ -22,13 +11,8 @@ keepAlive() {
   done
 }
 
-trunkPush() {
-  echo Pushing to Cocoapods trunk
-  login
-  pod trunk push --allow-warnings
-}
-
-justBuild() {
+testApp() {
+  touch /tmp/DontPlay
   echo Building the test app
   cd Example
   pod install
@@ -49,17 +33,11 @@ libLint() {
 
 FLAG=$(mktemp)
 
-if [[ $TRAVIS_TAG =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+if [ -n "$TRAVIS_TAG" ] || [ "$TRAVIS_EVENT_TYPE" == "cron" ]; then
   keepAlive $FLAG &
-  # If we're building a release tag (v1.2.3) push to cocoapods
-  trunkPush
-elif [ "$TRAVIS_EVENT_TYPE" == "cron" ]; then
-  keepAlive $FLAG &
-  # A cron build should do a full build (daily)
   libLint
 else
-  # Just build the test app (for every push and PR)
-  justBuild
+  testApp
 fi
 
 rm $FLAG  # stop keepAlive
