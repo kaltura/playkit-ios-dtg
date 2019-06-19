f90aa01fd0fa7e40a097fb70eb3704b9e83b42d3
Noam Tamim <noam.tamim@kaltura.com>
2019-06-19 22:07:53 +0300

    Update ContentManager.swift

--- Sources/ContentManager.swift
+++ Sources/ContentManager.swift
@@ -184,6 +184,29 @@ class DTGFilePaths {
     }
 }
 
+
+fileprivate class SafeSet<T: Hashable> {
+    private var set = Set<T>()
+    private let accessQueue = DispatchQueue(label: "SafeSet.accessQueue")
+
+    func add(_ member: T) -> Bool {
+        let oldMember = accessQueue.sync {
+            set.update(with: member)
+        }
+        return oldMember == nil
+    }
+
+    func remove(_ member: T) {
+        let removed = accessQueue.sync {
+            set.remove(member)
+        }
+        if removed == nil {
+            log.error("SafeSet.remove(): \(member) is missing from the set")
+        }
+    }
+}
+
+
 /* ***********************************************************/
 // MARK: - ContentManager
 /* ***********************************************************/
@@ -202,9 +225,11 @@ public class ContentManager: NSObject, DTGContentManager {
     public var referrer: String?
     
     // Set of items that are currently in the transient metadata-loading state.
-    private var metadataLoading = Set<String>()
+    private var metadataLoadingSet = SafeSet<String>()
     
     public weak var delegate: ContentManagerDelegate?
+    
+    
 
     public var storagePath: URL {
         return DTGFilePaths.storagePath
@@ -336,12 +361,14 @@ public class ContentManager: NSObject, DTGContentManager {
         
         // can only load metadata on item in `.new` state.
         guard item.state == .new else { throw DTGError.invalidState(itemId: id) }
-        if metadataLoading.contains(id) { throw DTGError.metadataLoading(itemId: id)}
         
-        metadataLoading.update(with: id)
+        if !metadataLoadingSet.add(id) {
+            throw DTGError.metadataLoading(itemId: id)
+        }
+        
         defer {
             log.debug("removing \(id) from metadataLoading")
-            metadataLoading.remove(id)  // done, with or without error
+            metadataLoadingSet.remove(id)  // done, with or without error
         }
         
         let referrer = (self.referrer == nil ? Bundle.main.bundleIdentifier ?? "" : self.referrer!).data(using: .utf8)?.base64EncodedString() ?? ""
