70940717f9ca2de9644ed473bc6b43c44c31fd36
Noam Tamim <noam.tamim@kaltura.com>
2020-05-03 12:34:09 +0300

    Ignore

---
+++ Tests/DownloadToGoTests/DownloadToGoTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import DownloadToGo
+
+final class DownloadToGoTests: XCTestCase {
+    func testExample() {
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import DownloadToGoTests
+
+var tests = [XCTestCaseEntry]()
+tests += DownloadToGoTests.allTests()
+XCTMain(tests)
